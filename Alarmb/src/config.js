533de568f0038cff2e689246f77cc45d4ee3342d
tladkat11 <tladkat11@gmail.com>
2024-10-10 10:58:36 +0530

    project structure change

--- Alarmb/src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  throttleTime: 5000,
-  cleanupInterval: 5 * 60 * 1000,
-  port: process.env.PORT || 8000,
-};
--- Alarmb/src/utils/permissionUtils.js
+++
@@ -1,7 +0,0 @@
-function hasAdminOrLeaderRole(member) {
-  return member.roles.cache.some(role => 
-    role.name.toLowerCase() === 'admin' || role.name.toLowerCase() === 'leader'
-  );
-}
-
-module.exports = { hasAdminOrLeaderRole };
