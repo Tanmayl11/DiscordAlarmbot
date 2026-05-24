d9069c46fadd83676c60421650412e74ec6805dc
Tanmayl11 <65541014+Tanmayl11@users.noreply.github.com>
2026-05-24 19:27:48 +0530

    Rename subcommand 'add-keyword-role' to 'add'

--- src/commands/watcherConfig.js
+++ src/commands/watcherConfig.js
@@ -126,7 +126,7 @@ function handleList(interaction, guildId) {
 }
 
 const SUB_HANDLERS = {
-  add: handleAdd,
+  "add-keyword-role": handleAdd,
   remove: handleRemove,
   list: handleList,
 };
@@ -140,7 +140,7 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .addSubcommand((sub) =>
       sub
-        .setName("add-keyword-role")
+        .setName("add")
         .setDescription("Add a rule: comma-separated keywords that ping comma-separated roles")
         .addStringOption((opt) =>
           opt
